d86ac210b136fa6e2a66e9f10370a3f4ab1ac5cf
Manyaka <104553245+237Codes@users.noreply.github.com>
2023-12-31 00:03:05 -0500

    create  smoother animation of project section

--- portfolio/components/project.tsx
+++ portfolio/components/project.tsx
@@ -2,7 +2,7 @@
 
 import { projectsData } from "@/lib/data";
 import Image from "next/image"; //This is used toimportthe image tothe relevant section
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 type ProjectsProps = (typeof projectsData)[number];
@@ -15,20 +15,24 @@ export default function Project({
 }: ProjectsProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
-    target: "",
+    target: ref,
     offset: ["0 1", "1.33 1"],
   });
+  // scale progress and opacity progress are being used  to hold the new values  transformed
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
     <motion.div
       ref={ref}
       style={{
-        scale: scrollYProgress,
-        opacity: scrollYProgress,
+        scale: scaleProgress,
+        opacity: opacityProgress,
       }}
-      className="mb-3 sm:mb-8 last:mb-0"
+      className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section
-        className="group
+        className="
             bg-gray-100 
             max-w-[42rem] 
             border 
@@ -42,7 +46,7 @@ export default function Project({
             transition"
       >
         <div
-          className="py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex
+          className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex
                 flex-col h-full group-even:ml-[18rem]"
         >
           <h3 className="text-2xl font-semibold">{title}</h3>
