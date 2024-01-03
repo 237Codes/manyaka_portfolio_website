47d9001b22568afba616ff45deda0a303b2ae414
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-01-03 07:13:07 -0500

    install dependencies for react vertical timeline component

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # manyaka_portfolio_website
 
 This is a document for my portfolio website. I will be documenting my progess here.
-
-4:30:00 - Creeting Experience Section
--- portfolio/components/experience.tsx
+++ portfolio/components/experience.tsx
@@ -1,10 +1,23 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+
 
 export default function Experience() {
   return (
     <section id="experience">
-      <SectionHeading>My Experience</SectionHeading>
+          <SectionHeading>My Experience</SectionHeading>
+          <VerticalTimeline>
+              
+          </VerticalTimeline>
     </section>
   );
 }
