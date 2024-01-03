d004f225a30b6678936bf336c8b021c575f53d7b
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-01-03 03:04:44 -0500

    add skills data

--- portfolio/lib/data.ts
+++ portfolio/lib/data.ts
@@ -88,6 +88,8 @@ export const projectsData = [
 ] as const;
 
 export const skillsData = [
+  "C++",
+  "Java",
   "HTML",
   "CSS",
   "JavaScript",
@@ -99,12 +101,11 @@ export const skillsData = [
   "Tailwind",
   "Prisma",
   "MongoDB",
-  "Redux",
   "GraphQL",
-  "Apollo",
   "Express",
   "PostgreSQL",
   "Python",
   "Django",
   "Framer Motion",
+  "Perspective",
 ] as const;
