ad30f624507e3711c9e0e17bc95de1b76f9ad347
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-01-12 04:02:09 +0100

    adjust layout page style

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
+import ThemeContextProvider from "@/context/theme-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,13 +36,15 @@ export default function RootLayout({
           w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33] lg:left-[-28rem] 
           xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[676394]"
         ></div>
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Footer />
-          <Toaster position="top-right" />
-        </ActiveSectionContextProvider>
-        <ThemeSwitch />
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
