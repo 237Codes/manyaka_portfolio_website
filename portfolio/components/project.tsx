ea37869e579a25aa74c621b92144abef7c218533
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-01-12 04:22:33 +0100

    set project images to hidden on smaller devices

--- portfolio/components/project.tsx
+++ portfolio/components/project.tsx
@@ -45,14 +45,14 @@ export default function Project({
             even:pl-8 
           hover:bg-gray-200 
             transition
-            group-even:pl-8
+            sm:group-even:pl-8
             dark:text-white
             dark:bg-white/10
             dark:hover:bg-white/20"
       >
         <div
           className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex
-                flex-col h-full group-even:ml-[18rem]"
+                flex-col h-full sm:group-even:ml-[18rem]"
         >
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
@@ -76,6 +76,8 @@ export default function Project({
           quality={95}
           className="
             absolute 
+            hidden
+            sm:block
             top-8 
             -right-40 
             w-[28.25rem] 
